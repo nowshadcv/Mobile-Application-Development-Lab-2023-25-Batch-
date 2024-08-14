ea53331ce5a228a9c6851effcf6b11e9c29aa663
jilsinaparveenkv <146071330+jilsinaparveenkv@users.noreply.github.com>
2024-08-14 12:03:16 +0530

    Add files via upload

---
+++ 22_JILSINA_PARVEEN_K_V/02-Login Form/02_MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText username=(EditText) findViewById(R.id.username);
+        EditText password=(EditText) findViewById(R.id.password);
+        Button button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("LINSHA") && password.getText().toString().equals("123456")){
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFULLY",Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED!!!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
