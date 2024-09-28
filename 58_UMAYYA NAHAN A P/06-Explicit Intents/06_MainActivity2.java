f7bc9f7003b7f1d7a069e6506212c64220997fc8
Umayya Nahan . A P <88381959+UmayyaNahanAP@users.noreply.github.com>
2024-09-27 22:22:48 -1200

    Add files via upload

---
+++ 58_UMAYYA NAHAN A P/06-Explicit Intents/06_MainActivity2.java
@@ -0,0 +1,41 @@
+package com.example.pgrm7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle bundle=getIntent().getExtras();
+        if (bundle != null) {
+            String imgindex=bundle.getString("ImageIndex");
+            setImage(imgindex);
+        }
+    }
+    private void setImage(String index){
+        ImageView imageView=(ImageView) findViewById(R.id.img1);
+        switch (index){
+            case "1":
+                imageView.setImageResource(R.drawable.apple);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.orange);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.grape);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.banana);
+                break;
+            default:
+                Toast.makeText(this, "Index is not available", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
