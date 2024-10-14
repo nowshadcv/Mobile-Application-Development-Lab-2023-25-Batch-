cc07859e11f6686818687e4d9062fb4a75731ee8
shabeebss <145668237+shabeebss@users.noreply.github.com>
2024-10-14 15:51:32 +0900

    Add files via upload

---
+++ 46_SHABEEB_AHAMMED_KT/06 - Explicit Intent/MainActivity2.java
@@ -0,0 +1,49 @@
+package com.example.explicit_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle bundle = getIntent().getExtras();
+        if(bundle!=null)
+        {
+            String imgindex=bundle.getString("ImageIndex");
+            SetImage(imgindex);
+        }
+    }
+
+    private void SetImage(String imgindex) {
+        ImageView imageview = (ImageView) findViewById(R.id.img1);
+
+        switch (imgindex)
+        {
+            case "1":
+                imageview.setImageResource(R.drawable.s23);
+                break;
+            case "2":
+                imageview.setImageResource(R.drawable.s24);
+                break;
+            case "3":
+                imageview.setImageResource(R.drawable.s22);
+                break;
+            case "4":
+                imageview.setImageResource(R.drawable.s21);
+                break;
+            default:
+                Toast.makeText(MainActivity2.this,"Wrong Input",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+
+}
