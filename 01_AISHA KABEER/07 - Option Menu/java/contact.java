84c1c9ff8711b6ead93436e1fda6e54fc9deb9ba
Aisha Kabeer <145667157+Aishakabeer@users.noreply.github.com>
2024-10-14 11:45:13 +0530

    Add files via upload

---
+++ 01_AISHA KABEER/07 - Option Menu/java/contact.java
@@ -0,0 +1,16 @@
+package com.example.option_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class contact extends AppCompatActivity {
+
+    public class Contactus extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_contact);
+        }
+    }}
