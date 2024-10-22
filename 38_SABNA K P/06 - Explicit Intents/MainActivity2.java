d9876e96650dd9511bbf2cafbc1af37b50e39078
sabbna <146524499+sabbna@users.noreply.github.com>
2024-10-22 15:19:16 +0530

    Add files via upload

---
+++ 38_SABNA K P/06 - Explicit Intents/MainActivity2.java
@@ -0,0 +1,44 @@
+package com.example.explicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle bundle = getIntent().getExtras();
+        if(bundle!=null)
+        {
+            String imgindex=bundle.getString("ImageIndex");
+            SetImage(imgindex);
+        }
+    }
+
+    private void SetImage(String imgindex) {
+        ImageView imageview = (ImageView) findViewById(R.id.img1);
+        switch (imgindex)
+        {
+            case "1":
+                imageview.setImageResource(R.drawable.apple);
+                break;
+            case "2":
+                imageview.setImageResource(R.drawable.banana);
+                break;
+            case "3":
+                imageview.setImageResource(R.drawable.orange);
+                break;
+            case "4":
+                imageview.setImageResource(R.drawable.mango);
+                break;
+            default:
+                imageview.setImageResource(R.drawable.image1);
+                break;
+        }
+    }
+
+}
