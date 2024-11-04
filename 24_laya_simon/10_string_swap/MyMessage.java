f37a33fc58ce6c46e47e25b87c1d6fbdae6f39b6
layasimon <146072706+layasimon@users.noreply.github.com>
2024-11-04 12:16:35 +0530

    Add files via upload

---
+++ 24_laya_simon/10_string_swap/MyMessage.java
@@ -0,0 +1,22 @@
+package com.example.stringswap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class MyMessage extends Activity {
+    TextView t;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Intent secondActivity = getIntent();
+        t=(TextView) findViewById(R.id.msg_text_view);
+        String result=secondActivity.getStringExtra("same");
+        t.setText(result);
+
+    }
+}
