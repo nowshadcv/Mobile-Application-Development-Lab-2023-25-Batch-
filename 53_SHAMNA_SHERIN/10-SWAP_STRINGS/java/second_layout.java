f47b97c082e65c62e51eed23cd15908489b6db11
shamna53 <146523370+shamna53@users.noreply.github.com>
2024-11-04 12:46:30 +0530

    Add files via upload

---
+++ 53_SHAMNA_SHERIN/10-SWAP_STRINGS/java/second_layout.java
@@ -0,0 +1,15 @@
+
+package com.example.swap_strings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class second_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
