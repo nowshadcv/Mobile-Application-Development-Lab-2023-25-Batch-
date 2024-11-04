f277cace5ce50f244f60b7f4627620034db662bc
Shamila51 <146522589+Shamila51@users.noreply.github.com>
2024-11-04 07:24:14 +0000

    Add files via upload

---
+++ 51_SHAMILA.VA/10-Swap_String/MyMessage.java
@@ -0,0 +1,22 @@
+package com.example.swapstring;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class MyMessage extends Activity {
+    TextView t;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        Intent secondActivity = getIntent();
+        t=(TextView) findViewById(R.id.msg_text_view);
+        String result=secondActivity.getStringExtra("same");
+        t.setText(result);
+
+    }
+}
