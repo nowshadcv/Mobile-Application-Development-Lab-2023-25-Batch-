8248b9409b1a92b26fc45cde909f790034bb3af5
muhd-shafeen-34 <145667235+muhd-shafeen-34@users.noreply.github.com>
2024-11-05 15:17:36 +0530

    Add files via upload

---
+++ 34_Muhammed_Shafeen_vk/07_OptionMenu/Java Files/terms_conditions.java
@@ -0,0 +1,14 @@
+package com.example.optionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class terms_conditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_conditions);
+    }
+}
