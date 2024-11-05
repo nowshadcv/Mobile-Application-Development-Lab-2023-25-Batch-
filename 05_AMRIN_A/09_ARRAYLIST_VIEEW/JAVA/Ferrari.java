e2edc1fa200a20eebdb780e8c48043cd2c48c57e
amrinaduvanni <145668094+amrinaduvanni@users.noreply.github.com>
2024-11-05 15:30:32 +0530

    Add files via upload

---
+++ 05_AMRIN_A/09_ARRAYLIST_VIEEW/JAVA/Ferrari.java
@@ -0,0 +1,14 @@
+package com.example.arraylistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ferrari extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ferrari);
+    }
+}
