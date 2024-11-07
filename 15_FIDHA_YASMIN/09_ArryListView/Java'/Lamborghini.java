b1858837e4db5ad6501b31f22f0a1f35efc13e52
fidhayasmin <145666933+fidhayasmin@users.noreply.github.com>
2024-11-07 10:35:53 +0000

    Add files via upload

---
+++ 15_FIDHA_YASMIN/09_ArryListView/Java'/Lamborghini.java
@@ -0,0 +1,14 @@
+package com.example.arraylistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lamborghini extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lamborghini);
+    }
+}
