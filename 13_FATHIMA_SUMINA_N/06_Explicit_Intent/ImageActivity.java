f151f149a29b3d69ffe96a486b7715c230ff5970
Fathima Sumina N <146462801+suminaah@users.noreply.github.com>
2024-11-07 10:33:10 +0000

    Add files via upload

---
+++ 13_FATHIMA_SUMINA_N/06_Explicit_Intent/ImageActivity.java
@@ -0,0 +1,41 @@
+package com.example.explicit_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Bundle bundle = getIntent().getExtras();
+        if(bundle!=null)
+        {
+            String imgindex=bundle.getString("ImageIndex");
+            SetImage(imgindex);
+        }
+    }
+
+    private void SetImage(String imgindex) {
+        ImageView imageview = (ImageView) findViewById(R.id.img1);
+        switch (imgindex)
+        {
+            case "1":
+                imageview.setImageResource(R.drawable.lion1);
+                break;
+            case "2":
+                imageview.setImageResource(R.drawable.tiger1);
+                break;
+            case "3":
+                imageview.setImageResource(R.drawable.elephant);
+                break;
+            case "4":
+                imageview.setImageResource(R.drawable.zebra);
+                break;
+        }
+    }
+
+}
